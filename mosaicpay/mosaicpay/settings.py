8dd350311ac1edd209efc9b8203462ffaa3ef244
Haris Kordić <kordicharis18@gmail.com>
2023-07-01 15:02:04 +0200

    feat: initial db setup AB#13

--- mosaicpay/mosaicpay/settings.py
+++ mosaicpay/mosaicpay/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mosaicpay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'your_host',  # Leave empty for localhost
+        'PORT': '5432',  # Default port is 5432
     }
 }
 
