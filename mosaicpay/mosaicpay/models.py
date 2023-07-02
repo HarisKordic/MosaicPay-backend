bfd98ed26a25cf102a5332287372d0e925c76fec
Haris Kordić <kordicharis18@gmail.com>
2023-07-02 14:32:56 +0200

    feat: initial test model AB#37

--- mosaicpay/mosaicpay/models.py
+++ mosaicpay/mosaicpay/models.py
@@ -1,1 +1,12 @@
-#Create entities here
+from django.db import models
+
+class Test(models.Model):
+    id = models.AutoField(primary_key=True)
+    value = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Test"
+        verbose_name_plural = "Tests"
+
+    def __str__(self):
+        return self.name
