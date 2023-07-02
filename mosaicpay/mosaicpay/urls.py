ca86ec87532ce565a3a883bf1aea5efa79126386
Haris Kordić <kordicharis18@gmail.com>
2023-07-02 14:50:34 +0200

    feat: test routes for docker AB#38

--- mosaicpay/mosaicpay/urls.py
+++ mosaicpay/mosaicpay/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from . import views
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import TestViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -11,8 +14,12 @@ schema_view = get_schema_view(
     ),
     public=True,
 )
+router = DefaultRouter()
+router.register(r'test', TestViewSet, basename='test')
+
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
